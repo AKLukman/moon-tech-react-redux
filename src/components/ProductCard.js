93edc7149d7ef9ae35f4d7628dd2f7a38259a0c8
AKLukman <abulkashemlukman@gmail.com>
2023-01-09 00:18:14 +0600

    done

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,34 +1,69 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import {
+  addToCart,
+  removeFromCart,
+} from "../redux/actionCreator/productAction";
+import { useLocation } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+  const { pathname } = useLocation();
   return (
     <div
-      className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
+      className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900 relative"
       key={product._id}
     >
-      <div className='h-52 w-52 mx-auto'>
+      {pathname.includes("cart") && (
+        <div className="bg-indigo-500 text-white h-8 w-8 rounded-full absolute top-2 right-2 grid place-items-center">
+          <p>{product.quantity}</p>
+        </div>
+      )}
+      <div className="h-52 w-52 mx-auto">
         <img src={product.image} alt={product.model} />
       </div>
-      <h1 className='font-bold text-center'>{product.model}</h1>
-      <p className='text-center font-semibold mb-3'>Rating: {product.rating}</p>
-      <div className=' flex-1'>
-        <ul className='space-y-2'>
+      <h1 className="font-bold text-center">{product.model}</h1>
+      <p className="text-center font-semibold mb-3">Rating: {product.rating}</p>
+      <div className=" flex-1">
+        <ul className="space-y-2">
           {product.keyFeature.map((feature) => {
-            return <li className='text-sm '>{feature}</li>;
+            return (
+              <li className="text-sm " key={feature}>
+                {feature}
+              </li>
+            );
           })}
         </ul>
       </div>
-      <div className='flex gap-2 mt-5'>
-        <button className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
-          Add to cart
-        </button>
-        <button
-          title='Add to wishlist'
-          className='bg-indigo-500  py-1 px-2 rounded-full'
-        >
-          <BiListPlus className='text-white' />
-        </button>
+      <div className="flex gap-2 mt-5">
+        {pathname.includes("cart") && (
+          <button
+            onClick={() => dispatch(removeFromCart(product))}
+            className="bg-red-500 flex justify-between px-2 items-center rounded-full py-1 px-2 flex-1 text-white text-bold"
+          >
+            <p className="text-lg font-bold">REMOVE </p>
+            <RiDeleteBin6Line size={20} />
+          </button>
+        )}
+        {!pathname.includes("cart") && (
+          <button
+            onClick={() => dispatch(addToCart(product))}
+            className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+          >
+            Add to cart
+          </button>
+        )}
+
+        {!pathname.includes("cart") && (
+          <button
+            title="Add to wishlist"
+            className="bg-indigo-500  py-1 px-2 rounded-full"
+          >
+            <BiListPlus className="text-white" />
+          </button>
+        )}
       </div>
     </div>
   );
